a76084eaf5e3a1eedd39628d79f096f528c3cb8b
ksundong <ksundong@gmail.com>
2018-10-28 18:59:57 +0900

    결합도가 높은 프로그램 예제
    이렇게 TV하나만 바꿨을 뿐인데 5가지나 바꿔야 한다.

--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -2,10 +2,10 @@ package polymorphism;
 
 public class TVUser {
 	public static void main(String[] args) {
-		SamsungTV tv = new SamsungTV();
-		tv.powerOn();
-		tv.volumeUp();
-		tv.volumeDown();
-		tv.powerOff();
+		LgTV tv = new LgTV();
+		tv.turnOn();
+		tv.soundUp();
+		tv.soundDown();
+		tv.turnOff();
 	}
 }

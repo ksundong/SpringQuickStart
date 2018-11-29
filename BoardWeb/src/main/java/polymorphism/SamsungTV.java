0bd4cd8a1782a5f1d232e81e4fbc3975b0e31e89
ksundong <ksundong@gmail.com>
2018-11-30 00:17:49 +0900

    bean 엘리먼트의 속성
    1) init-method 객체 생성시 Servlet의 init()같은 메소드가 필요해서 사용하는 것. 객체 초기화의 용도
    2) destroy-method 속성 스프링 컨테이너가 객체를 삭제하기 직전에 호출되는 메소드 지정
    3) lazy-init 속성 스프링은 pre-loading방식을 취해서 컨테이너가 구동될 때 bean들을 생성하는 즉시 불러오는데, 이를 bean을 호출할 때 불러오게 하는 것.
    4) scop속성 default는 singleton즉 bean에 해당하는 객체를 하나만 생성할 수 있도록 함. prototype으로 선언시 여러개가 생성되지만, 하나의 객체만 활성화 됨.(매번 새로운 객체 생성)

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -4,6 +4,9 @@ public class SamsungTV implements TV{
 	public SamsungTV() {
 		System.out.println("===> SamsungTV 객체 생성");
 	}
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리..");
+	}
 	public void powerOn() {
 		System.out.println("SamsungTV---전원 켠다.");
 	}
@@ -15,5 +18,8 @@ public class SamsungTV implements TV{
 	}
 	public void volumeDown() {
 		System.out.println("SamsungTV---소리 내린다.");
+	}
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전에 처리할 로직 처리...");
 	}
 }
--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -8,12 +8,14 @@ public class TVUser {
 		// 1. Spring 컨테이너를 구동한다.
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
 		
-		// 2. Spring 컨테이너로부터 필요한 객체르 요청(Lookup)한다.
-		TV tv = (TV)factory.getBean("tv");
-		tv.powerOn();
-		tv.volumeUp();
-		tv.volumeDown();
-		tv.powerOff();
+		// 2. Spring 컨테이너로부터 필요한 객체를 요청(Lookup)한다.
+		TV tv1 = (TV)factory.getBean("tv");
+		TV tv2 = (TV)factory.getBean("tv");
+		TV tv3 = (TV)factory.getBean("tv");
+//		tv.powerOn();
+//		tv.volumeUp();
+//		tv.volumeDown();
+//		tv.powerOff();
 		
 		// 3. Spring 컨테이너를 종료한다.
 		factory.close();

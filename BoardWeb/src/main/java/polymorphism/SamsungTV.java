66ce69570527450ce4ec0a46c51b8b36187bdd01
ksundong <ksundong@gmail.com>
2018-12-01 23:29:21 +0900

    의존성 주입 방법, 생성자 인젝션
    생성자 인젝션은 생성자의 매개변수를 오버라이딩하는 방법으로 컨테이너에서 지원한다.
    bean객체에서 constructor-arg 태그에 오버라이딩 시 필요로 하는 매개변수를 입력해주고, ref는 bean객체로 선언된 것을, value는 값을 가지는 객체일 경우
    index는 매개변수의 순서를 의미한다.

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,23 +1,35 @@
 package polymorphism;
 
 public class SamsungTV implements TV{
+	private Speaker speaker;
+	private int price;
+	
 	public SamsungTV() {
-		System.out.println("===> SamsungTV 객체 생성");
+		System.out.println("===> SamsungTV(1) 객체 생성");
+	}
+	public SamsungTV(Speaker speaker) {
+		System.out.println("===> SamsungTV(2) 객체 생성");
+		this.speaker = speaker;
+	}
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("===> SamsungTV(3) 객체 생성");
+		this.speaker = speaker;
+		this.price = price;
 	}
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리..");
 	}
 	public void powerOn() {
-		System.out.println("SamsungTV---전원 켠다.");
+		System.out.println("SamsungTV---전원 켠다. (가격 : " + price + ")");
 	}
 	public void powerOff() {
 		System.out.println("SamsungTV---전원 끈다.");
 	}
 	public void volumeUp() {
-		System.out.println("SamsungTV---소리 올린다.");
+		speaker.volumeUp();
 	}
 	public void volumeDown() {
-		System.out.println("SamsungTV---소리 내린다.");
+		speaker.volumeDown();
 	}
 	public void destroyMethod() {
 		System.out.println("객체 삭제 전에 처리할 로직 처리...");

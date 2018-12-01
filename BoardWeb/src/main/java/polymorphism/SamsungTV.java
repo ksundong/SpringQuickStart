cf640765755686f3c930239f3f00c69a352e51f3
ksundong <ksundong@gmail.com>
2018-12-01 23:42:59 +0900

    의존성 주입 두번째, Setter 인젝션
    1. property를 이용한 방법, property에 name은 매개변수명이라고 보면 되고(정확히는 Setter메소드 생성방식), 그 뒤에 ref나 value가 넣어줄 값을 의미
    2. p 네임스페이스를 이용한 방법, p:변수명-ref="참조할 객체의 name혹은 id" 또는 p:변수명="설정할 값"

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -7,15 +7,23 @@ public class SamsungTV implements TV{
 	public SamsungTV() {
 		System.out.println("===> SamsungTV(1) 객체 생성");
 	}
-	public SamsungTV(Speaker speaker) {
-		System.out.println("===> SamsungTV(2) 객체 생성");
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 호출");
 		this.speaker = speaker;
 	}
-	public SamsungTV(Speaker speaker, int price) {
-		System.out.println("===> SamsungTV(3) 객체 생성");
-		this.speaker = speaker;
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 호출");
 		this.price = price;
 	}
+	//	public SamsungTV(Speaker speaker) {
+//		System.out.println("===> SamsungTV(2) 객체 생성");
+//		this.speaker = speaker;
+//	}
+//	public SamsungTV(Speaker speaker, int price) {
+//		System.out.println("===> SamsungTV(3) 객체 생성");
+//		this.speaker = speaker;
+//		this.price = price;
+//	}
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리..");
 	}

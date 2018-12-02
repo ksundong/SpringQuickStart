e27352128b52d6968a1103f91fa6bb9ba9366abb
ksundong <ksundong@gmail.com>
2018-12-02 19:22:53 +0900

    프로젝트 진행을 위한 프로젝트 명 변경 및 진행 내용
    변경되지 않는 객체는 어노테이션으로 설정하여 사용하고, 변경될 가능성이 있는 객체는 XML설정으로 사용한다.

--- SpringExample/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
+++ SpringExample/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
@@ -1,7 +1,5 @@
 package com.springbook.ioc.injection;
 
-import java.util.Properties;
-
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -9,8 +7,8 @@ public class CollectionBeanClient {
 	public static void main(String[] args) {
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
 		
-		CollectionBean bean = (CollectionBean) factory.getBean("collectionBean");
-		Properties addressList = bean.getAddressList();
+		/*CollectionBean bean = (CollectionBean) factory.getBean("collectionBean");*/
+		/*Properties addressList = bean.getAddressList();*/
 /*		for (String address : addressList) {
 			System.out.println(address.toString());
 		}*/
--- SpringExample/src/main/java/polymorphism/LgTV.java
+++ SpringExample/src/main/java/polymorphism/LgTV.java
@@ -1,13 +1,11 @@
 package polymorphism;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("tv")
 public class LgTV implements TV{
 	@Autowired
-	@Qualifier("apple")
 	private Speaker speaker;
 	
 	public void powerOn() {

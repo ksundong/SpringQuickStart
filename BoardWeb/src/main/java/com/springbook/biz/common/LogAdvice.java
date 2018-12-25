c2e53cab817d11e0fc571a6fc34f6011777a9757
ksundong <ksundong@gmail.com>
2018-12-25 21:44:35 +0900

    JoinPoint 메소드 연습
    JoinPoint클래스는 getSignature, getTarget, get Args등의 유용한 메소드를 제공하고,
    getSignature가 제공하는 Signature객체를 이용하면, 호출되는 메소드에 대한 다양한 정보를 얻을 수 있다.
    다음은 Signature객체가 제공하는 메소드 중 일부이다.
    getName(): 호출한 메소드 이름 리턴, toLongString(): 상세한 정보 리턴, toShortString(): 메소드 정보 리턴

--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,7 +1,13 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class LogAdvice {
-	public void printLog() {
+	public void printLog(JoinPoint jp) {
+		Object[] args = jp.getArgs();
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args.toString());
+		}
 		System.out.println("[공통 로그] 비스니스 로직 수행 전 동작");
 	}
 }

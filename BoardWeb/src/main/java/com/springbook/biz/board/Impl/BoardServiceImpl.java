7e42a30e820e3ef51c7821d1acbaf6b5a10271e6
ksundong <ksundong@gmail.com>
2018-12-25 16:17:25 +0900

    Around Advice 설정 및 동작 테스트
    Around Advice는 하나의 Advice가 비즈니스 메소드 실행 전과 후에 모두 동작하여 로직을 처리한다.
    Around Advice는 클라이언트의 메소드 호출을 가로채어, 사전처리 로직을 수행할 수 있고, 비즈니스 로직 처리후에 사후 로직을 처리할 수도 있다.
    pjp.proceed() 메소드가 바로 비즈니스 로직을 호출하는 것이다. 즉, 이전의 코드는 Before와 같은 동작을 하고, 이후의 코드는 After와 같은 동작을 한다.
    이 Advice는 클라이언트가 비즈니스 메소드를 호출하는 것을 가로채기 위해 ProceedingJoinPoint 객체를 매개변수로 받아야 한다.
    그리고 그 객체의 proceed()메소드를 통해 비즈니스 메소드를 호출할 수 있다.
    Aspect 설정 역시 <aop:around> 엘리먼트를 applicationContext.xml에 선언해주어야한다. 그리고 예외발생 코드는 제거한다.

--- BoardWeb/src/main/java/com/springbook/biz/board/Impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/Impl/BoardServiceImpl.java
@@ -15,10 +15,6 @@ public class BoardServiceImpl implements BoardService {
 	
 	@Override
 	public void insertBoard(BoardVO vo) {
-		// 의도적으로 발생시킨 오류
-		if(vo.getSeq() == 0) {
-			throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
-		}
 		boardDAO.insertBoard(vo);
 	}
 

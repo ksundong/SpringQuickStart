52e43676ec3de431efa61c1e1e3dce87ea0d464b
ksundong <ksundong@gmail.com>
2019-02-27 23:57:57 +0900

    프리젠테이션 레이어와 비즈니스 레이어 통합
    DAO객체를 직접 접근하지 않고 Service 인터페이스를 통해 DAO의 역할이 바뀌더라도 Controller에 영향이 가지 않게끔 수정
    또한 AOP설정된 내용도 타게된다.
    그러나, 현재 설정으로는 동작하지 않는다. (왜냐하면 BoardService 타입의 객체가 메모리상에 없기 때문이다.)

--- BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,7 +1,8 @@
 package com.springbook.view.board;
 
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.board.Impl.BoardDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +15,9 @@ import java.util.Map;
 @Controller
 @SessionAttributes("board")
 public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	
 	// 검색 조건 목록 설정
 	@ModelAttribute("conditionMap")
 	public Map<String, String> searchConditionMap() {
@@ -26,14 +30,14 @@ public class BoardController {
 	
 	// 글 등록
 	@RequestMapping(value = "/insertBoard.do")
-	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.insertBoard(vo);
+	public String insertBoard(BoardVO vo) {
+		boardService.insertBoard(vo);
 		return "getBoardList.do";
 	}
 	
 	// 글 수정
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
 		{
 			System.out.println("번호 : " + vo.getSeq());
 			System.out.println("제목 : " + vo.getTitle());
@@ -41,29 +45,29 @@ public class BoardController {
 			System.out.println("내용 : " + vo.getContent());
 			System.out.println("등록일 : " + vo.getRegDate());
 			System.out.println("조회수 : " + vo.getCnt());
-			boardDAO.updateBoard(vo);
+			boardService.updateBoard(vo);
 			return "getBoardList.do";
 		}
 	}
 	
 	// 글 삭제
 	@RequestMapping("/deleteBoard.do")
-	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.deleteBoard(vo);
+	public String deleteBoard(BoardVO vo) {
+		boardService.deleteBoard(vo);
 		return "getBoardList.do";
 	}
 	
 	// 글 상세 조회
 	@RequestMapping("/getBoard.do")
-	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
+	public String getBoard(BoardVO vo, Model model) {
 		// Model 정보 저장
-		model.addAttribute("board", boardDAO.getBoard(vo)); // Model 정보 저장
+		model.addAttribute("board", boardService.getBoard(vo)); // Model 정보 저장
 		return "getBoard.jsp"; // View 이름 리턴
 	}
 	
 	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
+	public String getBoardList(BoardVO vo, Model model) {
 		String condition = vo.getSearchCondition();
 		String keyword = vo.getSearchKeyword();
 		
@@ -73,7 +77,7 @@ public class BoardController {
 		System.out.println("검색 조건 : " + condition);
 		System.out.println("검색 단어 : " + keyword);
 		// Model 정보 저장
-		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 		return "getBoardList.jsp";	// View 이름 리턴
 	}
 }
